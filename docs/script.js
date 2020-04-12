04cf578e194afc8d2a54b53b23205f6085eecd9d
Graicc <33105645+Graicc@users.noreply.github.com>
2020-04-12 19:25:35 -0400

    Updated site

--- docs/script.js
+++ docs/script.js
@@ -1,30 +1,23 @@
-function AddRow() {
-	//alert(elem.parentNode.children.length)
+function htmlToElement(html) {
+	var template = document.createElement('template')
+	template.innerHTML = html
+	return template.content.firstChild
+}
 
-	form = document.getElementById('form');
-	form.insertAdjacentHTML('afterbegin', `
-<div class="row">
+
+rowText = `<div class="row">
+<button type="button" onclick="RemoveRow(this)">-</button>
 <button type="button" onclick="AddColumn(this)">+</button>
-<button type="button" onclick="RemoveColumn(this)">-</button>
 <input type="text" id="keys" value="keys" />
-</div>
-`);	
-	AddColumn(form.children[0].children[0]);
-}
+</div>`
+newRow = htmlToElement(rowText)
 
-function RemoveRow() {
-	if (form.children.length > 4) {
-		form.removeChild(form.children[0]);
-	}		
-}
-
-function AddColumn(elem) {
-	elem.parentNode.insertAdjacentHTML('afterbegin', `
-<div class="pair">
+columnText = `<div class="pair">
+	<button type="button" onclick="RemoveColumn(this)">-</button>
 	<input name="Input Type" type="checkbox" id="type" onclick="InputChanged(this)">
-	<label for="type">Capacitive</label>
-	<input name="Input Type" type="checkbox" id="not" onclick="InputChanged(this)">
-	<label for="not">Not</label>
+	<label>Capacitive</label>
+	<input name="Input Type" type="checkbox" id="not">
+	<label>Not</label>
 
 
 	<select name="Input Edge" id="edge">
@@ -40,8 +33,8 @@ function AddColumn(elem) {
 		<option value="bY">                          Y                 </option>
 
 		<option value="bMenu" class="bOnly">         Menu              </option>
-		<option value="bLTrigger" class="bOnly">        Left Trigger      </option>
-		<option value="bRTrigger" class="bOnly">        Right Trigger     </option>
+		<option value="bLTrigger" class="bOnly">     Left Trigger      </option>
+		<option value="bRTrigger" class="bOnly">     Right Trigger     </option>
 		<option value="bLGrip" class="bOnly">        Left Grip         </option>
 		<option value="bRGrip" class="bOnly">        Right Grip        </option>
 
@@ -62,128 +55,144 @@ function AddColumn(elem) {
 		<option value="tLTrigger" class="tOnly">     Left Trigger      </option>
 		<option value="tRTrigger" class="tOnly">     Right Trigger     </option>
 	</select>
-</div>		
-`);
+</div>	
+`
+newColumn = htmlToElement(columnText)
 
-	InputChanged(elem.parentNode.children[0].children[0]);
+function insertAfter(newNode, referenceNode) {
+	referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling)
 }
 
-function RemoveColumn(elem) {
-	if (elem.parentNode.children.length > 4) {
-		elem.parentNode.removeChild(elem.parentNode.children[0]);
-	}
+function AddRow(addButton) {
+	newRowClone = newRow.cloneNode(true)
+	insertAfter(newRowClone, addButton)
+	AddColumn(newRowClone.childNodes[3])
+}
+
+function AddColumn(addButton) {
+	newColumnClone = newColumn.cloneNode(true)
+	insertAfter(newColumnClone, addButton)
+	InputChanged(newColumnClone.childNodes[0])
+}
+
+function RemoveRow(remButton) {
+	remButton.parentNode.remove()
+}
+
+function RemoveColumn(remButton) {
+	remButton.parentNode.remove()
 }
 
 function InputChanged(elem) {
-	var buttonInput = elem.parentNode.querySelector("#button");
-	elemType = elem.checked;
+	var buttonInput = elem.parentNode.querySelector("#button")
+	elemType = elem.checked
 	for (var i = 0; i < buttonInput.children.length; i++) {
-		var option = buttonInput.children[i];
-		optionChar = option.value;
-		optionType = option.className;
+		var option = buttonInput.children[i]
+		optionChar = option.value
+		optionType = option.className
 		option.disabled = (!elemType && optionType == "tOnly") || 
 											(elemType && optionType == "bOnly")
 	}
 	var temp = buttonInput.value
 	if (buttonInput.querySelector(`[value=${temp}]`).disabled) {
-		buttonInput.value = buttonInput.querySelector(":not([disabled])").value;
+		buttonInput.value = buttonInput.querySelector(":not([disabled])").value
 	}
 }
 
 function Submit() {
-	form = document.getElementById('form');
-	rows = form.querySelectorAll(':scope > div');
+	form = document.getElementById('form')
+	rows = form.querySelectorAll(':scope > div')
 	
 	fullText = ""
 	for (var i = 0; i < rows.length; i++) {
-		var row = rows[i];
+		var row = rows[i]
 
 		var text = ""
 
-		columns = row.querySelectorAll(':scope > .pair');
+		columns = row.querySelectorAll(':scope > .pair')
 		for (var j = 0; j < columns.length; j++) {
-			var column = columns[j];
+			var column = columns[j]
 			text += (column.querySelector('#type').checked ? "t" : "b")
 						 + column.querySelector('#edge').value + "|"
 						 + column.querySelector('#button').value + "|"
-						 +(column.querySelector('#not').checked ? "1" : "0") + ",";
+						 +(column.querySelector('#not').checked ? "1" : "0") + ","
 		}
 
-		keys = row.querySelector(':scope > #keys').value;
+		keys = row.querySelector(':scope > #keys').value
 		text += keys
 		fullText += text + "\r\n"
 	}
 
-	var link = document.getElementById('downloadlink');
-	 link.href = makeTextFile(fullText);
-	 link.style.display = 'block';
+	var link = document.getElementById('downloadlink')
+	 link.href = makeTextFile(fullText)
+	 link.style.display = 'block'
 }
 
-var textFile = null;
+var textFile = null
 makeTextFile = function (text) {
-	var data = new Blob([text], {type: 'text/plain'});
+	var data = new Blob([text], {type: 'text/plain'})
 
 	if (textFile !== null) {
-		window.URL.revokeObjectURL(textFile);
+		window.URL.revokeObjectURL(textFile)
 	}
 
-	textFile = window.URL.createObjectURL(data);
+	textFile = window.URL.createObjectURL(data)
 
-	return textFile;
-};
+	return textFile
+}
 
 function Load(files) {
 	// Add all of the files into a big string
- lines = "";
+ lines = ""
  for (var i = 0, f; f = files[i]; i++) {
 	 readFileContent(f).then(function(result) {
-		 lines = lines.concat(result, "\r\n");
+		 lines = lines.concat(result, "\r\n")
 	 })
  }
  setTimeout(() => {
-	 lines = lines.split("\r\n");
+	 lines = lines.split("\r\n")
 
 	 // Create binding object
-	 bindings = [];
+	 bindings = []
 	 for (var i = 0, l; l = lines[i]; i++) {
-		 sections = l.split(",");
+		 sections = l.split(",")
 		 binding = []
-		 binding.push(sections.pop());
+		 binding.push(sections.pop())
 		 for (var j = 0, s; s = sections[j]; j++) {
-			 binding.push(s.split("|"));
+			 binding.push(s.split("|"))
 		 }
-		 bindings.push(binding);
+		 bindings.push(binding)
 	 }
 
 	 // Clear out the current stuff
 	 while (form.children.length > 4) {
-		 RemoveRow();
+		 RemoveRow()
 	 }
 
 	 // Import binding object
 	 for (var i = bindings.length - 1, b; b = bindings[i]; i--) { // Items are added from bottom to top so do it in reverse for consistency.
 		 // Create new row
-		 AddRow(document.getElementById("add"));
-		 row = document.getElementsByClassName("row")[0];
+		 AddRow(document.getElementById("add"))
+		 row = document.getElementsByClassName("row")[0]
 
 		 // Add the correct number of columns
 		 for (var j = 2; j < b.length; j++) {
-			 AddColumn(row.childNodes[0]);
+			 AddColumn(row.childNodes[0])
 		 }
 
 		 // Loop over each pair
 		 for (var j = 1, p; p = b[j]; j++) {
-			 pair = row.children[j-1];
-			 pair.children[0].checked = p[0][0] == "t"; // Capacitive
-			 pair.children[2].checked = p[2] == 1; // Not
-			 pair.children[4].value = p[0].slice(1); // Edge
-			 pair.children[5].value = p[1]; // Button
+			 pair = row.children[j-1]
+			 pair.children[0].checked = p[0][0] == "t" // Capacitive
+			 pair.children[2].checked = p[2] == 1 // Not
+			 pair.children[4].value = p[0].slice(1) // Edge
+			 pair.children[5].value = p[1] // Button
 		 }
 
 		 // Add keystrokes
-		 row.children[row.children.length - 1].value = b[0];
+		 row.children[row.children.length - 1].value = b[0]
 	 }
- }, 100);
+ }, 100)
 }
 
 function readFileContent(file) { // This is wizardry and i don't know how it works
@@ -196,5 +205,5 @@ function readFileContent(file) { // This is wizardry and i don't know how it wor
 }
 
 window.onload = function() {
-	AddRow();
+	AddRow(document.getElementById("add"))
 };
