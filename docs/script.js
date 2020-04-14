8a534ee32b7b6a15567e3e8d54f7e42198c630e1
Graicc <33105645+Graicc@users.noreply.github.com>
2020-04-13 22:24:52 -0400

    Added Overwrite, fixed loading, reversed adding order

--- docs/script.js
+++ docs/script.js
@@ -4,29 +4,28 @@ function htmlToElement(html) {
 	return template.content.firstChild
 }
 
-
 rowText = `<div class="row">
-<button type="button" onclick="RemoveRow(this)">-</button>
-<button type="button" onclick="AddColumn(this)">+</button>
-<input type="text" id="keys" value="keys" />
+<button type="button" onclick="RemoveRow(this)" title="Remove Row">-</button>
+<button type="button" onclick="AddColumn(this)" title="Add Column">+</button>
+<input type="text" class="keys" value="keys" title="Keys">
 </div>`
 newRow = htmlToElement(rowText)
 
 columnText = `<div class="pair">
-	<button type="button" onclick="RemoveColumn(this)">-</button>
-	<input name="Input Type" type="checkbox" id="type" onclick="InputChanged(this)">
+	<button type="button" onclick="RemoveColumn(this)" title="Remove Column">-</button>
+	<input type="checkbox" class="type" onclick="InputChanged(this)" title="Capacitive">
 	<label>Capacitive</label>
-	<input name="Input Type" type="checkbox" id="not">
+	<input type="checkbox" class="not" title="Not">
 	<label>Not</label>
 
 
-	<select name="Input Edge" id="edge">
+	<select class="edge" title="Edge" title="Input Edge">
 		<option value="Pressed" selected>Pressed</option>
 		<option value="Released">Released</option>
 		<option value="Down">Down</option>
 	</select>
 
-	<select name="Input Button" id="button">
+	<select class="button" title="Input Button">
 		<option value="bA">                          A                 </option>
 		<option value="bB">                          B                 </option>
 		<option value="bX">                          X                 </option>
@@ -59,19 +58,17 @@ columnText = `<div class="pair">
 `
 newColumn = htmlToElement(columnText)
 
-function insertAfter(newNode, referenceNode) {
-	referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling)
-}
-
-function AddRow(addButton) {
-	newRowClone = newRow.cloneNode(true)
-	insertAfter(newRowClone, addButton)
+function AddRow() {
+	let newRowReferencePoint = document.getElementById("SubmitButton")
+	let newRowClone = newRow.cloneNode(true)
+	newRowReferencePoint.parentNode.insertBefore(newRowClone, newRowReferencePoint)
 	AddColumn(newRowClone.childNodes[3])
 }
 
 function AddColumn(addButton) {
-	newColumnClone = newColumn.cloneNode(true)
-	insertAfter(newColumnClone, addButton)
+	let newColumnReferencePoint = addButton.parentNode.lastElementChild
+	let newColumnClone = newColumn.cloneNode(true)
+	addButton.parentNode.insertBefore(newColumnClone, newColumnReferencePoint)
 	InputChanged(newColumnClone.childNodes[0])
 }
 
@@ -84,7 +81,7 @@ function RemoveColumn(remButton) {
 }
 
 function InputChanged(elem) {
-	var buttonInput = elem.parentNode.querySelector("#button")
+	var buttonInput = elem.parentNode.querySelector(".button")
 	elemType = elem.checked
 	for (var i = 0; i < buttonInput.children.length; i++) {
 		var option = buttonInput.children[i]
@@ -112,20 +109,20 @@ function Submit() {
 		columns = row.querySelectorAll(':scope > .pair')
 		for (var j = 0; j < columns.length; j++) {
 			var column = columns[j]
-			text += (column.querySelector('#type').checked ? "t" : "b")
-						 + column.querySelector('#edge').value + "|"
-						 + column.querySelector('#button').value + "|"
-						 +(column.querySelector('#not').checked ? "1" : "0") + ","
+			text += (column.querySelector('.type').checked ? "t" : "b")
+						 + column.querySelector('.edge').value + "|"
+						 + column.querySelector('.button').value + "|"
+						 +(column.querySelector('.not').checked ? "1" : "0") + ","
 		}
 
-		keys = row.querySelector(':scope > #keys').value
+		keys = row.querySelector(':scope > .keys').value
 		text += keys
 		fullText += text + "\r\n"
 	}
 
 	var link = document.getElementById('downloadlink')
-	 link.href = makeTextFile(fullText)
-	 link.style.display = 'block'
+	link.href = makeTextFile(fullText)
+	link.style.display = 'block'
 }
 
 var textFile = null
@@ -142,57 +139,67 @@ makeTextFile = function (text) {
 }
 
 function Load(files) {
+	overwrite = document.getElementById("overwrite").checked
+	console.log(overwrite)
+
 	// Add all of the files into a big string
- lines = ""
- for (var i = 0, f; f = files[i]; i++) {
-	 readFileContent(f).then(function(result) {
-		 lines = lines.concat(result, "\r\n")
-	 })
- }
- setTimeout(() => {
-	 lines = lines.split("\r\n")
+	lines = ""
+	for (var i = 0, f; f = files[i]; i++) {
+		readFileContent(f).then(function(result) {
+			lines = lines.concat(result, "\r\n")
+	 	})
+ 	}
+ 	setTimeout(() => {
+		lines = lines.split("\r\n")
 
-	 // Create binding object
-	 bindings = []
-	 for (var i = 0, l; l = lines[i]; i++) {
-		 sections = l.split(",")
-		 binding = []
-		 binding.push(sections.pop())
-		 for (var j = 0, s; s = sections[j]; j++) {
-			 binding.push(s.split("|"))
-		 }
-		 bindings.push(binding)
-	 }
+		// Create binding object
+		bindings = []
+		for (var i = 0, l; l = lines[i]; i++) {
+			sections = l.split(",")
+			binding = []
+			binding.push(sections.pop())
+			for (var j = 0, s; s = sections[j]; j++) {
+				binding.push(s.split("|"))
+			}
+			bindings.push(binding)
+		}
 
-	 // Clear out the current stuff
-	 while (form.children.length > 4) {
-		 RemoveRow()
-	 }
+		// Clear out the current stuff
+		if (overwrite) {
+			while (document.getElementsByClassName("Row").length > 0) {
+				RemoveRow(document.getElementsByClassName("Row")[0].firstChild)
+			}
+		}
 
-	 // Import binding object
-	 for (var i = bindings.length - 1, b; b = bindings[i]; i--) { // Items are added from bottom to top so do it in reverse for consistency.
-		 // Create new row
-		 AddRow(document.getElementById("add"))
-		 row = document.getElementsByClassName("row")[0]
 
-		 // Add the correct number of columns
-		 for (var j = 2; j < b.length; j++) {
-			 AddColumn(row.childNodes[0])
-		 }
+		//while (form.children.length > 4) { It's a feature that it dosen't clear it
+		//	RemoveRow()
+		//}
 
-		 // Loop over each pair
-		 for (var j = 1, p; p = b[j]; j++) {
-			 pair = row.children[j-1]
-			 pair.children[0].checked = p[0][0] == "t" // Capacitive
-			 pair.children[2].checked = p[2] == 1 // Not
-			 pair.children[4].value = p[0].slice(1) // Edge
-			 pair.children[5].value = p[1] // Button
-		 }
+		// Import binding object
+		for (var i = 0, b; b = bindings[i]; i++) {
+			// Create new row
+			AddRow()
+			let row = document.getElementsByClassName("row")[document.getElementsByClassName("row").length-1]
 
-		 // Add keystrokes
-		 row.children[row.children.length - 1].value = b[0]
-	 }
- }, 100)
+			// Add the correct number of columns
+			for (var j = 2; j < b.length; j++) {
+				AddColumn(row.firstChild)
+			}
+
+			// Loop over each pair
+			for (var j = 0, p; p = b[j+1]; j++) {
+				pair = row.children[j+2]
+				pair.getElementsByClassName("type")[0].checked = p[0][0] == "t" // Capacitive
+				pair.getElementsByClassName("not")[0].checked = p[2] == 1 // Not
+				pair.getElementsByClassName("edge")[0].value = p[0].slice(1) // Edge
+				pair.getElementsByClassName("button")[0].value = p[1] // Button
+			}
+
+			// Add keystrokes
+			row.children[row.children.length - 1].value = b[0]
+		}
+ 	}, 100)
 }
 
 function readFileContent(file) { // This is wizardry and i don't know how it works
